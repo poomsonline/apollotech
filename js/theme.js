1fb64ef2c43ea919b749f1be53a20829e8369fc1
Sharjeel Rajput <sharjeel@truemeridian.com>
2018-09-23 00:26:16 +0500

    new update

--- js/theme.js
+++ js/theme.js
@@ -20,6 +20,32 @@
         };
     };
     navbarFixed();
+
+    /*---------------------------------------------------------------------
+    Scroll Navigation
+    ---------------------------------------------------------------------*/
+    function scrollNav() {
+        $('nav a').click(function(){  
+        //Toggle Class
+        console.log('Clicked');
+        $(".active").removeClass("active");      
+        $(this).closest('li').addClass("active");
+        var theClass = $(this).attr("class");
+        $('.'+theClass).parent('li').addClass('active');
+        //Animate
+        $('html, body').stop().animate({
+            scrollTop: $( $(this).attr('href') ).offset().top - 160
+        }, 400);
+        return false;
+        });
+        $('.scrollTop a').scrollTop();
+    }
+
+    $(document).ready(function(){
+        console.log("Called");
+        scrollNav();
+    });
+    
     
     
     /*----------------------------------------------------*/
